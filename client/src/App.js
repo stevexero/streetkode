430b3a2901d4c15e8c1846b350329e8aaea19f6b
Steve <steveanthony999@gmail.com>
2022-10-05 01:11:15 -0700

    added nav

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
