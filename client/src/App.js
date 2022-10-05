49b6ac15bc5dbe52121e3f8c9fc5b275712ad329
Steve <steveanthony999@gmail.com>
2022-10-05 01:34:11 -0700

    set up 404

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 import Navbar from './components/Navbar';
 
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </Router>
   );
