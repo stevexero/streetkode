e20a25e4a881f0b474911507479100d53591c4d7
Steve <steveanthony999@gmail.com>
2022-10-07 03:01:09 -0700

    set up cart button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import { RiShoppingCartLine } from 'react-icons/ri';
+
 import { logout } from '../features/auth/authSlice';
 import { openAuthModal } from '../features/modals/modalSlice';
 
@@ -17,6 +19,10 @@ const Navbar = () => {
     dispatch(logout());
   };
 
+  const showCart = () => {
+    console.log('cart');
+  };
+
   return (
     <nav style={{ backgroundColor: 'lightgrey' }}>
       <ul>
@@ -29,6 +35,12 @@ const Navbar = () => {
           ) : (
             <button onClick={handleLoginClick}>Login</button>
           )}
+        </li>
+        <li>
+          <button onClick={showCart}>
+            <RiShoppingCartLine />
+            <p>0</p>
+          </button>
         </li>
       </ul>
     </nav>
